c06cf634e7565ae0adac060fbc2e7b7ae5d3088e
kvazimode <kvazimode2@gmail.com>
2021-03-04 16:22:37 +0300

    initial

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app/app.jsx';
+
+const project = {
+  name: ``
+}
+
+ReactDOM.render(
+  <App setup={setup}/>,
+  document.getElementById('root')
+)
