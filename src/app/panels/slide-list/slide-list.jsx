d0772b38a5d167f44aedb5eb06ff03585a2b2710
kvazimode <kvazimode2@gmail.com>
2021-03-12 12:09:20 +0300

    slide list

--- src/app/panels/slide-list/slide-list.jsx
+++ src/app/panels/slide-list/slide-list.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import Controls from './controls.jsx'
+import Slides from './slides.jsx'
+
+const slides = [
+  {
+    name: `Slide 001`,
+    bg: `bgName1.png`
+  },
+  {
+    name: `Slide 002`,
+    bg: `bgName2.png`
+  }
+]
 
 export default () => {
   return <React.Fragment>
     <div className="panel panel-slidelist">
       slides
       <Controls />
+      <Slides slides={slides} />
     </div>
   </React.Fragment>
 }
---
+++ src/app/panels/slide-list/slide.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default (props) => {
+    return (
+        <div className="slide">
+            <span className="slide-name">{props.count + 1}. {props.slide.name}</span>
+            <br/>
+            <span className="slide-bg">{props.slide.bg}</span>
+        </div>
+    )
+}
