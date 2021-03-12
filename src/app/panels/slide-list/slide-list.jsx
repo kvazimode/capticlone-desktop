c690e8dc16d0ae0d8015666abfd2d997389b6fa8
kvazimode <kvazimode2@gmail.com>
2021-03-12 11:29:39 +0300

    slide controls

--- src/app/panels/slide-list/slide-list.jsx
+++ src/app/panels/slide-list/slide-list.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Controls from './controls.jsx'
 
 export default () => {
   return <React.Fragment>
     <div className="panel panel-slidelist">
       slides
+      <Controls />
     </div>
   </React.Fragment>
 }
