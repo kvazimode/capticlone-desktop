b55adbd3be9d1f896830bd9fe03ba13f56f985e8
kvazimode <kvazimode2@gmail.com>
2021-03-22 11:13:45 +0300

    transition block

--- src/app/panels/params/basic.jsx
+++ src/app/panels/params/basic.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Timing from './timing.jsx'
 import Position from './position.jsx'
+import Transition from './transition.jsx'
 
 export default function Basic() {
     return (<form>
         <Timing />
         <Position />
+        <Transition />
     </form>)
 }
