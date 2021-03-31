5371edde8be0052c156075a223ec5dbd04630c5c
kvazimode <kvazimode2@gmail.com>
2021-03-31 17:26:23 +0300

    fieldset instead of div

--- src/app/panels/slide-list/controls.jsx
+++ src/app/panels/slide-list/controls.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 export default function Controls() {
     return (
-        <div className="block slides-block">
+        <fieldset>
+            <legend>Управление слайдами</legend>
             <button className="button-slides button-slides--add">До</button>
             <button className="button-slides button-slides--delete">Уд</button>
             <button className="button-slides button-slides--up">Вв</button>
             <button className="button-slides button-slides--down">Вн</button>
             <button className="button-slides button-slides--rename">Пе</button>
             <button className="button-slides button-slides--copy">Ко</button>
-        </div>
+        </fieldset>
     )
 }
