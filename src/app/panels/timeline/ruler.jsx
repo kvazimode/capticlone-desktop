6fa3ce04ad514fce0f7ce7d44b8e49c1e3d0d040
kvazimode <kvazimode2@gmail.com>
2021-03-31 16:05:34 +0300

    ruler component

---
+++ src/app/panels/timeline/ruler.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Ruler() {
+  return (
+    <div className="ruler">
+        <label>0</label>
+        <label>2</label>
+        <label>4</label>
+        <label>6</label>
+        <label>8</label>
+        <label>10</label>
+        <label>12</label>
+        <label>14</label>
+        <label>16</label>
+        <label>18</label>
+        <label>20</label>
+    </div>
+  )
+}
--- src/app/panels/timeline/timeline.jsx
+++ src/app/panels/timeline/timeline.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Ruler from './ruler.jsx'
 
 export default function Timeline() {
   return (
     <div className="panel panel-timeline">
       timeline
+      <Ruler />
     </div>
   )
 }
