203d8f86aaeff2069d3c343a8d3eea88cfc45cab
kvazimode <kvazimode2@gmail.com>
2021-04-08 17:43:57 +0300

    drawing bg on canvas

--- src/app/canvas/canvas.jsx
+++ src/app/canvas/canvas.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 
-export default function Canvas() {
+export default function Canvas(props) {
+  const bgImg = new Image()
+  bgImg.src = `./img/${props.bgImg}`
+  const canvasRef = useRef(null)
+
+  function drawBg(ctx) {
+    ctx.drawImage(bgImg, 0, 0, 1280, 720)
+  }
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const ctx = canvas.getContext(`2d`)
+    drawBg(ctx)
+  }, [drawBg])
+
   return <>
     <div className="panel panel-canvas">
-      <canvas className="canvas" width="1280" height="720"></canvas>
+      <canvas ref={canvasRef} className="canvas" width="1280" height="720"></canvas>
     </div>
     </>
 }
