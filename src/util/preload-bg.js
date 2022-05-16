2147d47eb50babab170e678dd174bd3b996ac967
kvazimode <kvazimode2@gmail.comn>
2022-05-16 10:22:26 +0300

    dont remember what i was writing. remember issue with image load. after building standalone app issue was gone. just turned off web security
     On branch file
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   public/electron.js
    	modified:   src/app/app.jsx
    	modified:   src/index.js
    	modified:   src/util/preload-bg.js

--- src/util/preload-bg.js
+++ src/util/preload-bg.js
@@ -1,13 +1,14 @@
-const preloadBg = (proj) => {
+const preloadBg = (proj, projectPath) => {
     return new Promise((res, rej) => {
         const preloaded = []
         proj.slides.map(slide => {
             if (slide.bgImg == null) return
             const img = new Image()
-            img.src = `./img/${slide.bgImg}`
+            img.src = `${projectPath}/${slide.bgImg}`
             img.slideID = slide.id
             preloaded.push(img)
         })
+        console.log(preloaded)
         res(preloaded)
     })
 }
